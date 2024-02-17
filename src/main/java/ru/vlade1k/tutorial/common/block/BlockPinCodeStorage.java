48b1bc15a8eb28efb0cb397c779bc15591f92fc4
vlade1k <vladode2015@yandex.ru>
2024-02-17 11:21:18 +0300

    add texture to Pin-Code Storage

--- src/main/java/ru/vlade1k/tutorial/common/block/BlockPinCodeStorage.java
+++ src/main/java/ru/vlade1k/tutorial/common/block/BlockPinCodeStorage.java
@@ -1,15 +1,22 @@
 package ru.vlade1k.tutorial.common.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
 import ru.vlade1k.tutorial.TutorialMod;
 import ru.vlade1k.tutorial.common.gui.pin.PinCodeGui;
 import ru.vlade1k.tutorial.common.handler.ModTab;
@@ -19,6 +26,9 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class BlockPinCodeStorage extends BlockContainer {
+
+  private IIcon blockIcon;
+  private IIcon frontBlockIcon;
 
   private final Random rand = new Random();
   public BlockPinCodeStorage() {
@@ -62,6 +72,38 @@ public class BlockPinCodeStorage extends BlockContainer {
       item.setVelocity((rand.nextDouble() - 0.5) * 0.25, rand.nextDouble() * 0.5 * 0.25, (rand.nextDouble() - 0.5) * 0.25);
       world.spawnEntityInWorld(item);
     }
+  }
+
+  @SideOnly(Side.CLIENT)
+  @Override
+  public void registerBlockIcons(IIconRegister register) {
+    blockIcon = register.registerIcon(TutorialMod.MOD_ID + ":block-pincode");
+    frontBlockIcon = register.registerIcon(TutorialMod.MOD_ID + ":block-pincode-face");
+  }
+
+  @SideOnly(Side.CLIENT)
+  @Override
+  public IIcon getIcon(int side, int meta) {
+    ForgeDirection direction = ForgeDirection.getOrientation(side);
+    ForgeDirection blockDirection;
+
+    if (meta != 0) {
+      blockDirection = ForgeDirection.getOrientation(meta);
+    } else {
+      blockDirection = ForgeDirection.WEST;
+    }
+
+    if (direction == blockDirection) return frontBlockIcon;
+    return blockIcon;
+  }
+
+  @Override
+  public void onBlockPlacedBy(World world, int x, int y, int z,
+      EntityLivingBase entity, ItemStack itemStack) {
+    super.onBlockPlacedBy(world, x, y, z, entity, itemStack);
+    int dir = (MathHelper.floor_double((double)(entity.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3);
+    int[] r = {2, 5, 3, 4};
+    world.setBlockMetadataWithNotify(x, y, z, r[dir], 3);
   }
 
   public TileEntity createNewTileEntity(World world, int par2) {
